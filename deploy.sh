5a5def9d76e2b566fc0cb3e92fbaf3d557dc5633
Lovelyleti20 <letisianji89@gmail.come>
2024-06-07 21:20:43 +0000

    updated deploy created

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-Application is ready to be deployed to the development enviroment
-
+Application is ready to be deployed to the development enviromentei
+best engineers ever
 deployment must be reviewed
 deployment must be reviewed by managers
 deployment must be reviewed by manager before developments
